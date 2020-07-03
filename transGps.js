6747ec0e702e35018807eddf52d58a065fdebe22
uc22 <51921920+uc22@users.noreply.github.com>
2020-07-03 16:30:25 +0800

    Update transGps.js

--- transGps.js
+++ transGps.js
@@ -117,3 +117,25 @@ function transformlng(lng, lat) {
 function out_of_china(lng, lat) {
     return (lng < 72.004 || lng > 137.8347) || ((lat < 0.8293 || lat > 55.8271) || false);
 }
+
+       function azimuth(lng_a, lat_a, lng_b, lat_b) {
+            var a = (90 - lat_b) * Math.PI / 180;
+            var b = (90 - lat_a) * Math.PI / 180;
+            var AOC_BOC = (lng_b - lng_a) * Math.PI / 180;
+            var cosc = Math.cos(a) * Math.cos(b) + Math.sin(a) * Math.sin(b) * Math.cos(AOC_BOC);
+            var sinc = Math.sqrt(1 - cosc * cosc);
+            var sinA = Math.sin(a) * Math.sin(AOC_BOC) / sinc;
+            var A = Math.asin(sinA) * 180 / Math.PI;
+            var res = 0;
+            if (lng_b > lng_a && lat_b > lat_a) res = A;
+            else if (lng_b > lng_a && lat_b < lat_a) res = 180 - A;
+            else if (lng_b < lng_a && lat_b < lat_a) res = 180 - A;
+            else if (lng_b < lng_a && lat_b > lat_a) res = 360 + A;
+            else if (lng_b > lng_a && lat_b == lat_a) res = 90;
+            else if (lng_b < lng_a && lat_b == lat_a) res = 270;
+            else if (lng_b == lng_a && lat_b > lat_a) res = 0;
+            else if (lng_b == lng_a && lat_b < lat_a) res = 180;
+
+            return res;
+
+        };
